772b878090f14799e2a4a2992e28966a06a59a25
Gustavo Mathias <gmgustavom@gmail.com>
2022-08-01 18:54:19 -0300

    fix: env variable on token

--- app/backend/src/providers/implementations/JWTTokenProvider.ts
+++ app/backend/src/providers/implementations/JWTTokenProvider.ts
@@ -3,12 +3,11 @@ import * as jwt from 'jsonwebtoken';
 import { ITokenProvider, tokenData } from '../ITokenProvider';
 
 export default class JWtTokenProvider implements ITokenProvider {
-  private secret = process.env.SECRET || 'tokenSecreto';
+  private secret = process.env.JWT_SECRET || 'jwt_secret';
 
   generateToken({ id, email, username }: tokenData): string {
     const token = jwt.sign({ id, email, username }, this.secret, {
-      expiresIn: '15m',
-      algorithm: 'HS256',
+      expiresIn: '15m'
     });
 
     return token;
