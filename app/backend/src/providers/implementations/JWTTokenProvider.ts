eec2b2851ea613123058e1ee7698e51dda759d65
Gustavo Mathias <gmgustavom@gmail.com>
2022-08-02 09:00:52 -0300

    feat: implementing decodeToken
    implementing decodeToken on jwt token provider

--- app/backend/src/providers/implementations/JWTTokenProvider.ts
+++ app/backend/src/providers/implementations/JWTTokenProvider.ts
@@ -1,15 +1,20 @@
 import 'dotenv/config';
 import * as jwt from 'jsonwebtoken';
-import { ITokenProvider, tokenData } from '../ITokenProvider';
+import { ITokenProvider, ITokenData } from '../ITokenProvider';
 
 export default class JWtTokenProvider implements ITokenProvider {
   private secret = process.env.JWT_SECRET || 'jwt_secret';
 
-  generateToken({ id, email, username }: tokenData): string {
+  generateToken({ id, email, username }: ITokenData): string {
     const token = jwt.sign({ id, email, username }, this.secret, {
-      expiresIn: '15m'
+      expiresIn: '15m',
     });
 
     return token;
   }
+
+  decodeToken = (token: string): ITokenData => {
+    const tokenData = jwt.decode(token) as ITokenData;
+    return tokenData;
+  };
 }
