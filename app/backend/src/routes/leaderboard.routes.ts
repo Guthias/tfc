15ceda75cb1e4b2d761a06abdb916dfea45df243
Gustavo Mathias <gmgustavom@gmail.com>
2022-08-08 21:37:04 -0300

    fix: updating routes order

--- app/backend/src/routes/leaderboard.routes.ts
+++ app/backend/src/routes/leaderboard.routes.ts
@@ -4,11 +4,6 @@ import { getLeaderboardController } from '../useCases/GetLeaderboard';
 const router = Router();
 
 router.get(
-  '/',
-  async (req: Request, res: Response) => getLeaderboardController.handleLeaderboard(req, res),
-);
-
-router.get(
   '/home',
   async (req: Request, res: Response) => getLeaderboardController.handleHomeLeaderboard(req, res),
 );
@@ -16,6 +11,11 @@ router.get(
 router.get(
   '/away',
   async (req: Request, res: Response) => getLeaderboardController.handleAwayLeaderboard(req, res),
+);
+
+router.get(
+  '/',
+  async (req: Request, res: Response) => getLeaderboardController.handleLeaderboard(req, res),
 );
 
 export default router;
